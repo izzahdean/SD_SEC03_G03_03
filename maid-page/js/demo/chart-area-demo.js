1d75449060bd54f85d3c735826890949448417ef
zafiraqistina <qistinazafiraa@gmail.com>
2024-09-11 23:11:18 +0800

    edit earnings (maid)

--- maid-page/js/demo/chart-area-demo.js
+++ maid-page/js/demo/chart-area-demo.js
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500],
     }],
   },
   options: {
