5bc3fd7714916c92d08790c574e85671de23cd6c
NURUL IZZAH BAHARUDIN <nrllzzhh04@gmail.com>
2024-09-11 22:55:42 +0800

    navigate

--- register.php
+++ register.php
@@ -123,7 +123,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>MYKAKAKS Guest - Register</title>
+        <title>MYKAKAKS Guest - Sign Up</title>
         <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
         <link href="styling.css" rel="stylesheet">
@@ -169,7 +169,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                             </div>
                                         </div>
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Register Account
+                                            Sign Up
                                         </button>
                                         <hr>
                                     </form>
