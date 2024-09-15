6afdbc021120e97e61acdcef6925691b8da0e2d0
NURUL IZZAH BAHARUDIN <nrllzzhh04@gmail.com>
2024-09-16 00:34:27 +0800

    create strong password

--- register.php
+++ register.php
@@ -14,7 +14,7 @@ use PHPMailer\PHPMailer\Exception;
 // Database connection parameters
 $servername = "localhost";
 $username = "wp2024";
-$password = "@webprogramming"; // Update with your MySQL password
+$password = "@webprogramming"; 
 $dbname = "mysister";
 
 // Create connection
@@ -41,9 +41,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST['password'];
     $repeat_password = $_POST['repeat_password'];
 
-    // Password validation: Check if password is at least 8 characters long
-    if (strlen($password) < 8) {
-        echo "Password must be at least 8 characters long!";
+    // Server-side password validation
+    if (strlen($password) < 8 || !preg_match('/[A-Z]/', $password) || !preg_match('/[a-z]/', $password) || !preg_match('/[0-9]/', $password) || !preg_match('/[@$!%*?&]/', $password)) {
+        echo "Password must be at least 8 characters long, contain an uppercase letter, a lowercase letter, a number, and a special character!";
         exit();
     }
 
@@ -82,20 +82,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         try {
             // Server settings
-            $mail->isSMTP(); // Set mailer to use SMTP
-            $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
-            $mail->SMTPAuth = true; // Enable SMTP authentication
-            $mail->Username = 'nrulizzh35@gmail.com'; // SMTP username
-            $mail->Password = 'qaml lehv vntq cqur'; // SMTP password
-            $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
-            $mail->Port = 587; // TCP port to connect to
+            $mail->isSMTP(); 
+            $mail->Host = 'smtp.gmail.com'; 
+            $mail->SMTPAuth = true; 
+            $mail->Username = 'nrulizzh35@gmail.com'; 
+            $mail->Password = 'qaml lehv vntq cqur'; 
+            $mail->SMTPSecure = 'tls'; 
+            $mail->Port = 587; 
 
             // Recipients
             $mail->setFrom('nrulizzh35@gmail.com', 'MyKakaks');
-            $mail->addAddress($email); // Add a recipient
+            $mail->addAddress($email); 
 
             // Content
-            $mail->isHTML(true); // Set email format to HTML
+            $mail->isHTML(true); 
             $mail->Subject = 'Your account authentication Code';
             $mail->Body    = 'Your OTP code is: ' . $verification_code;
             $mail->AltBody = 'Your OTP code is: ' . $verification_code;
@@ -115,67 +115,92 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->close();
     $conn->close();
 } else {
-    // Display the registration form
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>MYKAKAKS Guest - Sign Up</title>
-        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-        <link href="styling.css" rel="stylesheet">
-    </head>
-    <body class="bg-gradient-primary">
-        <div class="container">
-            <div class="card o-hidden border-0 shadow-lg my-5">
-                <div class="card-body p-0">
-                    <div class="row">
-                        <div class="col-xl-6 d-flex align-items-center justify-content-center">
-                            <img src="image/login.png" alt="Logo" class="img-fluid">
-                        </div><br>
-                        <div class="col-lg-6 d-flex align-items-center justify-content-center">
-                            <div class="p-4 w-100">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4"><b>Create an Account!</b></h1>
+    // Display the registration form with the password validation
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>MYKAKAKS Guest - Sign Up</title>
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="styling.css" rel="stylesheet">
+    <style>
+        .password-checklist {
+            display: none; /* Initially hidden */
+            font-size: 14px; /* Adjust the font size as needed */
+			margin-right: 120px;
+        }
+        .password-checklist input[type="checkbox"] {
+            margin-right: 10px;
+        }
+        .password-checklist li {
+            margin-bottom: 5px; /* Add some spacing between items */
+        }
+        .password-checklist li.valid {
+            color: green;
+        }
+        .password-checklist li.invalid {
+            color: red;
+        }
+    </style>
+</head>
+<body class="bg-gradient-primary">
+    <div class="container">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <div class="row">
+                    <div class="col-xl-6 d-flex align-items-center justify-content-center">
+                        <img src="image/login.png" alt="Logo" class="img-fluid">
+                    </div><br>
+                    <div class="col-lg-6 d-flex align-items-center justify-content-center">
+                        <div class="p-4 w-100">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4"><b>Create an Account!</b></h1>
+                                </div>
+                                <form id="signupForm" class="user" action="register.php" method="POST">
+                                    <div class="form-group row">
+                                        <div class="col-sm-6 mb-3 mb-sm-0">
+                                            <input type="text" class="form-control form-control-user" id="firstName" name="first_name" placeholder="First Name" required>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <input type="text" class="form-control form-control-user" id="lastName" name="last_name" placeholder="Last Name" required>
+                                        </div>
                                     </div>
-                                    <form id="signupForm" class="user" action="register.php" method="POST">
-                                        <div class="form-group row">
-                                            <div class="col-sm-6 mb-3 mb-sm-0">
-                                                <input type="text" class="form-control form-control-user" id="firstName" name="first_name" placeholder="First Name" required>
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <input type="text" class="form-control form-control-user" id="lastName" name="last_name" placeholder="Last Name" required>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="tel" class="form-control form-control-user" id="contactNumber" name="contact_number" placeholder="Contact Number" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" id="address" name="address" placeholder="Address" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" required>
-                                        </div>
-                                        <div class="form-group row">
-                                            <div class="col-sm-6 mb-3 mb-sm-0">
-                                                <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password (atleast 8 characters)" required>
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <input type="password" class="form-control form-control-user" id="repeatPassword" name="repeat_password" placeholder="Confirm Password" required>
-                                            </div>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Sign Up
-                                        </button>
-                                        <hr>
-                                    </form>
-                                    <div class="text-center">
-                                        <a class="small" href="login.php">Already have an account? Login!</a>
+                                    <div class="form-group">
+                                        <input type="tel" class="form-control form-control-user" id="contactNumber" name="contact_number" placeholder="Contact Number" required>
                                     </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user" id="address" name="address" placeholder="Address" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" required>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-sm-6 mb-3 mb-sm-0">
+                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password" required>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <input type="password" class="form-control form-control-user" id="repeatPassword" name="repeat_password" placeholder="Confirm Password" required>
+                                        </div>
+                                    </div>
+                                    <ul class="password-checklist">
+                                        <li><input type="checkbox" id="length" disabled> At least 8 characters</li>
+                                        <li><input type="checkbox" id="uppercase" disabled> At least one uppercase letter</li>
+                                        <li><input type="checkbox" id="lowercase" disabled> At least one lowercase letter</li>
+                                        <li><input type="checkbox" id="number" disabled> At least one number</li>
+                                        <li><input type="checkbox" id="special" disabled> At least one special character</li>
+                                    </ul>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block" id="submitBtn" disabled>
+                                        Sign Up
+                                    </button>
+                                    <hr>
+                                </form>
+                                <div class="text-center">
+                                    <a class="small" href="login.php">Already have an account? Login!</a>
                                 </div>
                             </div>
                         </div>
@@ -183,33 +208,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </div>
             </div>
         </div>
-        <script src="vendor/jquery/jquery.min.js"></script>
-        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-        <script src="js/sb-admin-2.min.js"></script>
+    </div>
 
-        <!-- Client-side password validation -->
-        <script>
-            document.getElementById('signupForm').addEventListener('submit', function (event) {
-                var password = document.getElementById('password').value;
-                var repeatPassword = document.getElementById('repeatPassword').value;
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="js/sb-admin-2.min.js"></script>
 
-                // Check if the password is at least 8 characters long
-                if (password.length < 8) {
-                    alert('Password must be at least 8 characters long.');
-                    event.preventDefault(); // Prevent the form from being submitted
-                    return;
-                }
+    <script>
+        const passwordInput = document.getElementById('password');
+        const repeatPasswordInput = document.getElementById('repeatPassword');
+        const checklist = {
+            length: document.getElementById('length'),
+            uppercase: document.getElementById('uppercase'),
+            lowercase: document.getElementById('lowercase'),
+            number: document.getElementById('number'),
+            special: document.getElementById('special'),
+        };
+        const passwordChecklist = document.querySelector('.password-checklist');
+        const submitBtn = document.getElementById('submitBtn');
 
-                // Check if passwords match
-                if (password !== repeatPassword) {
-                    alert('Passwords do not match!');
-                    event.preventDefault();
-                }
-            });
-        </script>
-    </body>
-    </html>
-    <?php
+        // Show checklist when user starts typing
+        passwordInput.addEventListener('focus', function () {
+            passwordChecklist.style.display = 'block';
+        });
+
+        passwordInput.addEventListener('input', function () {
+            const password = passwordInput.value;
+
+            // Check for each validation
+            checklist.length.checked = password.length >= 8;
+            checklist.uppercase.checked = /[A-Z]/.test(password);
+            checklist.lowercase.checked = /[a-z]/.test(password);
+            checklist.number.checked = /\d/.test(password);
+            checklist.special.checked = /[@$!%*?&]/.test(password);
+
+            // Enable the submit button if all requirements are met
+            const allValid = Object.values(checklist).every(item => item.checked);
+            submitBtn.disabled = !allValid;
+        });
+
+        // Optionally hide the checklist when the input loses focus
+        passwordInput.addEventListener('blur', function () {
+            if (passwordInput.value === '') {
+                passwordChecklist.style.display = 'none';
+            }
+        });
+    </script>
+</body>
+</html>
+<?php
 }
-?>
