1fc413d1b2bb6a85cd3cd4fccba239e874ae0c6f
NURUL IZZAH BAHARUDIN <nrllzzhh04@gmail.com>
2024-10-09 20:52:16 +0800

    edit cust

--- admin-page/customer.php
+++ admin-page/customer.php
@@ -1,3 +1,49 @@
+<?php
+session_start();
+
+include '../connect-db.php';
+
+if (isset($_GET['delete_id'])) {
+    $customerId = $_GET['delete_id'];
+
+    $emailSql = "SELECT email FROM customer WHERE id = ?";
+    $emailStmt = $conn->prepare($emailSql);
+    $emailStmt->bind_param("i", $customerId);
+    $emailStmt->execute();
+    $emailResult = $emailStmt->get_result();
+    
+    if ($emailResult->num_rows > 0) {
+        $row = $emailResult->fetch_assoc();
+        $customerEmail = $row['email'];
+
+        $deleteCustomerSql = "DELETE FROM customer WHERE id = ?";
+        $customerStmt = $conn->prepare($deleteCustomerSql);
+        $customerStmt->bind_param("i", $customerId);
+        $customerStmt->execute();
+
+        $deleteUserSql = "DELETE FROM users WHERE email = ?";
+        $userStmt = $conn->prepare($deleteUserSql);
+        $userStmt->bind_param("s", $customerEmail);
+        $userStmt->execute();
+
+        if ($customerStmt->affected_rows > 0 && $userStmt->affected_rows > 0) {
+            echo "<script>alert('Customer and corresponding user deleted successfully.'); window.location.href = 'customer.php';</script>";
+        } else {
+            echo "<script>alert('Error deleting customer or user: " . $conn->error . "');</script>";
+        }
+
+        $customerStmt->close();
+        $userStmt->close();
+    } else {
+        echo "<script>alert('Customer not found.');</script>";
+    }
+    
+    $emailStmt->close();
+}
+
+$sql = "SELECT id, fname, lname, email, address, status FROM customer";
+$result = $conn->query($sql);
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,50 +57,40 @@
 
     <title>MYKAKAKS Admin - Customer</title>
 
-    <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-    <!-- Custom styles for this template-->
     <link href="css/mykakaks-admin.css" rel="stylesheet">
 
 </head>
 
 <body id="page-top">
 
-    <!-- Page Wrapper -->
     <div id="wrapper">
 
-        <!-- Sidebar -->
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                   <img src="img/logo.png" style="width: 800px; height: 30px;">
                 <div class="sidebar-brand-text mx-3">ADMIN</div>
             </a>
 
-            <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
-            <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
                 <a class="nav-link" href="index.html">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
 
-            <!-- Divider -->
             <hr class="sidebar-divider">
 
-            <!-- Heading -->
             <div class="sidebar-heading">
                 Menu
             </div>
 
-            <!-- Nav Item - User Management Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
@@ -64,12 +100,11 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <a class="collapse-item" href="maid.php">Maid</a>
-                        <a class="collapse-item" href="customer.html">Customer</a>
+                        <a class="collapse-item" href="customer.php">Customer</a>
                     </div>
                 </div>
             </li>
 
-            <!-- Nav Item - Content Management Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
@@ -84,7 +119,6 @@
                 </div>
             </li>
 			
-			<!-- Nav Item - Service Status Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseService"
                     aria-expanded="true" aria-controls="collapseService">
@@ -99,65 +133,50 @@
                 </div>
             </li>
 
-            <!-- Divider -->
             <hr class="sidebar-divider">
 
-            <!-- Heading -->
             <div class="sidebar-heading">
                 Others
             </div>
 			
-			<!-- Nav Item - Rating -->
             <li class="nav-item">
                 <a class="nav-link" href="feedback.html">
                     <i class="fas fa-fw fa-star"></i>
                     <span>Feedback</span></a>
             </li>
 
-            <!-- Nav Item - Report -->
             <li class="nav-item">
                 <a class="nav-link" href="charts.html">
                     <i class="fas fa-fw fa-download"></i>
                     <span>Generate Report</span></a>
             </li>
 
-            <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
 
-            <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
 
         </ul>
-        <!-- End of Sidebar -->
 
-        <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Main Content -->
             <div id="content">
 
-                <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-                    <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                         <i class="fa fa-bars"></i>
                     </button>
 
-                    <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
-                        <!-- Nav Item - Alerts -->
                         <li class="nav-item dropdown no-arrow mx-1">
                             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-bell fa-fw"></i>
-                                <!-- Counter - Alerts -->
                                 <span class="badge badge-danger badge-counter">3+</span>
                             </a>
-                            <!-- Dropdown - Alerts -->
                             <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="alertsDropdown">
                                 <h6 class="dropdown-header">
@@ -200,15 +219,12 @@
                             </div>
                         </li>
 
-                        <!-- Nav Item - Messages -->
                         <li class="nav-item dropdown no-arrow mx-1">
                             <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-envelope fa-fw"></i>
-                                <!-- Counter - Messages -->
                                 <span class="badge badge-danger badge-counter">7</span>
                             </a>
-                            <!-- Dropdown - Messages -->
                             <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="messagesDropdown">
                                 <h6 class="dropdown-header">
@@ -268,7 +284,6 @@
 
                         <div class="topbar-divider d-none d-sm-block"></div>
 
-                        <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -276,7 +291,6 @@
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
-                            <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
                                 <a class="dropdown-item" href="profile.php">
@@ -298,9 +312,7 @@
                     </ul>
 
                 </nav>
-                <!-- End of Topbar -->
 
-                <!-- Begin Page Content -->
                 <div class="container-fluid">
 					<div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">MyKakaks Customer</h1>
@@ -314,80 +326,48 @@
 							<div class="card-body">
 								<div class="table-responsive">
 									<table class="table table-striped">
-										<thead class="thead-light">
-											<tr>
-												<th scope="col">Name</th>
-												<th scope="col">Email</th>
-												<th scope="col">Address</th>
-												<th scope="col">Subscription</th>
-												<th scope="col">Actions</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr>
-												<td><img src="https://i.pravatar.cc/40?u=1" class="rounded-circle" alt="avatar">&nbsp;Customer Name</td>
-												<td>emailcust@gmail.com</td>
-												<td>No.12, Jalan kampung pisang,
-													Bukit Utama</td>
-												<td><span class="subscription-status status-active"><i class="fas fa-circle"></i> Active</span></td>
-												<td class="actions">
-													<i class="fas fa-edit action-icons text-primary"></i>
-													<i class="fas fa-trash action-icons text-danger"></i>
-												</td>
-											</tr>
-											<tr>
-												<td><img src="https://i.pravatar.cc/40?u=2" class="rounded-circle" alt="avatar"> Customer Name</td>
-												<td>emailcust@gmail.com</td>
-												<td>No.5, Jalan Kent 1, Permai Jaya</td>
-												<td><span class="subscription-status status-active"><i class="fas fa-circle"></i> Active</span></td>
-												<td class="actions">
-													<i class="fas fa-edit action-icons text-primary"></i>
-													<i class="fas fa-trash action-icons text-danger"></i>
-												</td>
-											</tr>
-											<tr>
-												<td><img src="https://i.pravatar.cc/40?u=3" class="rounded-circle" alt="avatar"> Customer Name</td>
-												<td>emailcust@gmail.com</td>
-												<td>No.33, Jalan BJ 1/2, Bukit Mewah</td>
-												<td><span class="subscription-status status-active"><i class="fas fa-circle"></i> Active</span></td>
-												<td class="actions">
-													<i class="fas fa-edit action-icons text-primary"></i>
-													<i class="fas fa-trash action-icons text-danger"></i>
-												</td>
-											</tr>
-											<tr>
-												<td><img src="https://i.pravatar.cc/40?u=4" class="rounded-circle" alt="avatar"> Customer Name</td>
-												<td>emailcust@gmail.com</td>
-												<td>Block A, Jalan PPR 101, Gombak</td>
-												<td><span class="subscription-status status-inactive"><i class="fas fa-circle"></i> Inactive</span></td>
-												<td class="actions">
-													<i class="fas fa-edit action-icons text-primary"></i>
-													<i class="fas fa-trash action-icons text-danger"></i>
-												</td>
-											</tr>
-											<tr>
-												<td><img src="https://i.pravatar.cc/40?u=5" class="rounded-circle" alt="avatar"> Customer Name</td>
-												<td>emailcust@gmail.com</td>
-												<td>No.10, Jalan Permata 5, Taman Permata</td>
-												<td><span class="subscription-status status-inactive"><i class="fas fa-circle"></i> Inactive</span></td>
-												<td class="actions">
-													<i class="fas fa-edit action-icons text-primary"></i>
-													<i class="fas fa-trash action-icons text-danger"></i>
-												</td>
-											</tr>
-										</tbody>
-									</table>
+                                        <thead class="thead-light">
+                                            <tr>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Address</th>
+                                                <th scope="col">Status</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            // Display fetched data in the table
+                                            if ($result->num_rows > 0) {
+                                                while ($row = $result->fetch_assoc()) {
+                                                    echo "<tr>";
+                                                    echo "<td>" . htmlspecialchars($row['fname']) . " " . htmlspecialchars($row['lname']) . "</td>";
+                                                    echo "<td>" . htmlspecialchars($row['email']) . "</td>";
+                                                    echo "<td>" . htmlspecialchars($row['address']) . "</td>";
+                                                    echo "<td>" . htmlspecialchars($row['status']) . "</td>";
+                                                    echo "<td>
+													    <a href='edit-cust.php?id=" . $row['id'] . "' class='btn btn-warning btn-sm'><i class='fas fa-edit'></i> Edit</a>
+                                                        <a href='customer.php?delete_id=" . $row['id'] . "' onclick=\"return confirm('Are you sure you want to delete this customer?');\" class='btn btn-danger btn-sm'>
+                                                            <i class='fas fa-trash'></i> Delete
+                                                        </a>
+                                                      </td>";
+                                                    echo "</tr>";
+                                                }
+                                            } else {
+                                                echo "<tr><td colspan='5'>No customers found.</td></tr>";
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-                <!-- /.container-fluid -->
+
 
             </div>
-            <!-- End of Main Content -->
 
-            <!-- Footer -->
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
@@ -395,20 +375,15 @@
                     </div>
                 </div>
             </footer>
-            <!-- End of Footer -->
 
         </div>
-        <!-- End of Content Wrapper -->
 
     </div>
-    <!-- End of Page Wrapper -->
 
-    <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
---
+++ admin-page/edit-cust.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+include '../connect-db.php';
+
+if (isset($_GET['id'])) {
+    $customerId = $_GET['id'];
+
+    $sql = "SELECT * FROM customer WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $customerId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $customer = $result->fetch_assoc();
+    } else {
+        echo "<script>alert('Customer not found.'); window.location.href = 'customer.php';</script>";
+        exit;
+    }
+
+    $stmt->close();
+} else {
+    echo "<script>alert('Invalid request.'); window.location.href = 'customer.php';</script>";
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $status = $_POST['status'];
+
+    $updateCustomerSql = "UPDATE customer SET status = ? WHERE id = ?";
+    $updateCustomerStmt = $conn->prepare($updateCustomerSql);
+    $updateCustomerStmt->bind_param("si", $status, $customerId);
+
+    if ($updateCustomerStmt->execute()) {
+        header("Location: customer.php");
+            exit;
+    } else {
+        echo "<script>alert('Error updating customer: " . $conn->error . "');</script>";
+    }
+
+    $updateCustomerStmt->close();
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Edit Customer Status</title>
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="css/mykakaks-admin.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container mt-5">
+        <h2>Edit Customer Status</h2>
+        <form method="POST" action="edit-cust.php?id=<?php echo $customerId; ?>">
+            <div class="form-group">
+                <label for="status">Status:</label>
+                <select class="form-control" id="status" name="status" required>
+                    <option value="active" <?php echo ($customer['status'] === 'active') ? 'selected' : ''; ?>>Active</option>
+                    <option value="inactive" <?php echo ($customer['status'] === 'inactive') ? 'selected' : ''; ?>>Inactive</option>
+                </select>
+            </div>
+            <button type="submit" class="btn btn-primary">Update Status</button>
+            <a href="customer.php" class="btn btn-secondary">Cancel</a>
+        </form>
+    </div>
+
+</body>
+</html>
