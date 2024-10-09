74f50a6e79411a664db0ec3915de24ca97d00ec7
ardinee <ardineemohamadzaid@gmail.com>
2024-10-09 21:23:50 +0800

    title icon

--- admin-page/profile.php
+++ admin-page/profile.php
@@ -49,6 +49,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="shortcut icon" href="img/favicon.png" type="">
     <title>Admin Profile</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
