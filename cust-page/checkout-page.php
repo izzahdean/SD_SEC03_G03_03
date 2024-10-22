f88cc9e03def06edffda35a5a27c702474784071
ardinee <ardineemohamadzaid@gmail.com>
2024-10-22 09:56:53 +0800

    rename css

--- cust-page/checkout-page.php
+++ cust-page/checkout-page.php
@@ -28,7 +28,7 @@
   <link href="css/font-awesome.min.css" rel="stylesheet" />
 
   <!-- Custom styles for this template -->
-  <link href="css/style.css" rel="stylesheet" />
+  <link href="csscheckout/style.css" rel="stylesheet" />
   <!-- responsive style -->
   <link href="css/responsive.css" rel="stylesheet" />
   
